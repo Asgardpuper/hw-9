a35c055653fa196bffa673c5289266d5f9b14be8
Asgardpuper <maks.kachalay@gmail.com>
2022-11-17 20:03:14 +0200

    add task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,31 @@
+const refs = {
+  form: document.querySelector('.form'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const { delay, step, amount } = event.currentTarget;
+  const delayVl = Number(delay.value);
+  const stepVl = Number(step.value);
+  const amountVl = Number(amount.value);
+  for (let position = 0; position < amountVl; position++) {
+    let timeDelay = delayVl + stepVl * position;
+    createPromise(position, timeDelay)
+      .then(message => console.log(message))
+      .catch(error => console.log(error));
+  }
+}
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position + 1} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position + 1} in ${delay}ms`);
+      }
+    }, delay);
+  });
 }
